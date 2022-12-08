e8b2970c5ab1ccb9a1ceca93ab536226660cbed3
Rasmus <49008491+Eklund89@users.noreply.github.com>
2022-12-08 20:53:43 +0100

    implemented set resolution button and reset button

--- script.js
+++ script.js
@@ -1,22 +1,46 @@
-let container = document.querySelector('.container');
 
-const side = 16;
-let counter = 0;
-for (let row = 0; row < side; row++) {
-    let rowBox = document.createElement('div');
-    rowBox.classList.add('row')
-    for (let col = 0; col < side; col++) {
-        let box = document.createElement('div');
-        box.classList.add('box')
-        box.setAttribute('id', counter++)
-        rowBox.appendChild(box)
+
+drawCanvas(64, 400);
+const reset = document.querySelector('.reset')
+const resolution = document.querySelector('.resolution')
+reset.onclick = () => resetCanvas();
+resolution.onclick = () => drawCanvas(prompt("Please enter desired resolution", 64), 400)
+
+
+function drawCanvas(res, size) {
+    const container = document.querySelector('.container');
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
     }
-    container.appendChild(rowBox)
+    boxSize = (size / res).toFixed(0);
+    let counter = 0;
+    for (let row = 0; row < res; row++) {
+        let rowBox = document.createElement('div');
+        rowBox.classList.add('row');
+        for (let col = 0; col < res; col++) {
+            let box = document.createElement('div');
+            box.classList.add('box');
+            box.setAttribute('id', counter++);
+            box.style.cssText = `width: ${boxSize}px; height: ${boxSize}px`;
+            rowBox.appendChild(box);
+        }
+        container.appendChild(rowBox);
+    }
+    addBoxEvents();
 }
 
-const boxes = document.querySelectorAll('.box');
-boxes.forEach((box) => {
-    box.addEventListener('mouseover', (e) => {
-        e.target.classList.add('marked')
+function addBoxEvents() {
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach((box) => {
+        box.addEventListener('mouseover', (e) => {
+            e.target.classList.add('marked');
+        });
     });
-});
+}
+
+function resetCanvas() {
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach((box) => {
+        box.classList.remove('marked');
+    })
+}
