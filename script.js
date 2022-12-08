120d4799a5cf20260dd0a36dacb60578d5233cee
Rasmus <49008491+Eklund89@users.noreply.github.com>
2022-12-08 22:37:34 +0100

    implemented select options

--- script.js
+++ script.js
@@ -3,9 +3,12 @@
 drawCanvas(64, 400);
 const reset = document.querySelector('.reset')
 const resolution = document.querySelector('.resolution')
+const select = document.querySelector('select')
 reset.onclick = () => resetCanvas();
 resolution.onclick = () => drawCanvas(prompt("Please enter desired resolution", 64), 400)
+select.addEventListener('change', () => {
 
+});
 
 function drawCanvas(res, size) {
     const container = document.querySelector('.container');
@@ -21,7 +24,7 @@ function drawCanvas(res, size) {
             let box = document.createElement('div');
             box.classList.add('box');
             box.setAttribute('id', counter++);
-            box.style.cssText = `width: ${boxSize}px; height: ${boxSize}px`;
+            box.style.cssText = `width: ${boxSize}px; height: ${boxSize}px; background-color: rgb(255,255,255)`;
             rowBox.appendChild(box);
         }
         container.appendChild(rowBox);
@@ -33,7 +36,11 @@ function addBoxEvents() {
     const boxes = document.querySelectorAll('.box');
     boxes.forEach((box) => {
         box.addEventListener('mouseover', (e) => {
-            e.target.classList.add('marked');
+            if (e.buttons === 1) {
+                const option = select.options[select.selectedIndex].value;
+                const color = e.target.style.backgroundColor;
+                e.target.style.backgroundColor = setColor(option, color);
+            }
         });
     });
 }
@@ -41,6 +48,22 @@ function addBoxEvents() {
 function resetCanvas() {
     const boxes = document.querySelectorAll('.box');
     boxes.forEach((box) => {
-        box.classList.remove('marked');
+        box.style.backgroundColor = 'rgb(255,255,255)'
     })
 }
+
+function setColor(option, current) {
+    let out;
+    if (option === 'black') {
+        out = 'rgb(0,0,0)';
+    } else if (option === 'random') {
+        out = `rgb(${randomNumber()},${randomNumber()},${randomNumber()})`
+    } else if (option === 'tenPercent') {
+        current = parseInt(current.match(/(\d{1,3})\)/)[1])
+        newColor = Math.max(1, Math.min(parseInt(current - 25), 255))
+        out = `rgb(${newColor}, ${newColor}, ${newColor})`
+    }
+    return out;
+}
+
+let randomNumber = () => Math.floor(Math.random() * 255);
